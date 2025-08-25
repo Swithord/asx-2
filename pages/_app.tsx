1a9d7e8282d6c4df83cace9a8a327d97cb60f0c3
York Ng <yorkhayng@gmail.com>
2025-08-25 06:03:10 -0400

    fix name maybe?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import "@/styles/globals.css";
 import '@/styles/fonts.css';
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>UofT Astronomy & Space Exploration Association</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
