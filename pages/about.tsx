e859e4f0a839afad3258e56f7b75fb7b4b67bfb2
York Ng <yorkhayng@gmail.com>
2025-08-27 02:05:42 -0400

    fix about ui bugs

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,10 +11,10 @@ import { Button } from "@/components/ui/button";
 export default function About() {
   return (
     <div className='max-w-7xl mx-auto'>
-      <div className='container flex flex-col items-center justify-between min-h-screen mx-auto gap-5 sm:gap-10 md:gap-15'>
         <Navbar />
-        <div className='flex-grow flex flex-col gap-5 px-4 sm:gap-10 md:gap-15 items-center'>
-            <div className="w-full flex justify-center items-center max-h-128">
+      <div className='container flex flex-col items-center justify-between min-h-screen mx-auto gap-5 sm:gap-10 md:gap-15'>
+        <div className='flex-grow flex flex-col gap-10 px-4 md:gap-10 items-center'>
+            <div className="w-full flex justify-center items-center max-h-128 overflow-hidden">
                 <div className="w-full max-w-[1280px]">
                     <Image
                         src={asx2425}
@@ -25,7 +25,7 @@ export default function About() {
                     />
                 </div>
             </div>
-            <div className='flex flex-col gap-5 w-full items-center pt-10'>
+            <div className='flex flex-col gap-5 w-full items-center'>
                 <div className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 px-4 bg-black'>
                   <div className='text-3xl md:text-4xl'>Our Mission</div>
                 </div>
@@ -48,7 +48,7 @@ export default function About() {
                 <div className='text-lg md:text-xl'>Since its inception in 2003, ASX has rapidly grown, organising numerous high-profile events with distinguished speakers, including astronauts, astronomers, and space entrepreneurs. ASX has also established partnerships with various organizations, including the Royal Astronomical Society of Canada, the Canadian Space Society, and the Canadian Space Agency.</div>
             </div>
             <div className='w-full flex flex-col gap-15 md:flex-row md:justify-between'>
-                <div className='flex flex-col gap-3 items-center md:max-w-1/4'>
+                <div className='flex flex-col gap-3 items-center pt-10 md:pt-0 md:max-w-1/4'>
                     <div className='text-xl md:text-2xl text-primary'>Looking to sponsor us?</div>
                     <div className='text-lg md:text-xl text-center'>
                        ASX relies heavily on external sponsorships in order to continue informing today's society about astronomy and space exploration. By sponsoring us, you or your company can be involved with a non-profit organization that strives to share the excitement of space with the public!
