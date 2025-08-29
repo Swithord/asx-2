320985038ef99f968f2145b338419d65fea06d0f
York Ng <yorkhayng@gmail.com>
2025-08-29 17:33:24 -0400

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import '@/styles/fonts.css';
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { Toaster } from "@/components/ui/sonner"
+import { Analytics } from '@vercel/analytics/next';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <Component {...pageProps} />
       <Toaster />
+      <Analytics />
     </>
   );
 }
